8f3e843c542693c6963fd75e85d057aa45f52369
Andrew Rawson <arawson@proton.me>
2025-03-08 13:20:24 -0500

    removed redundant comments, added print statements for formatting output

--- randomMessageGenerator/main.js
+++ randomMessageGenerator/main.js
@@ -6,9 +6,8 @@
 // It will be a terminal program, because we are working with basic concepts
 // This makes me want to lean toward a prompt
 // It also could just run from initialization without user input, but that seems less fun
+import readline from 'readline';
 
-import readline from 'readline'; // I will use readline to get user input
-// I will use the factory function to generate messages
 const messageFactory = () => {
     const part1 = ['foo', 'bar', 'baz', 'qux'];
     const part2 = ['hello', 'world', 'foo', 'bar'];
@@ -25,39 +24,36 @@ const userInput = (input, rl) => {
         console.log(''); // Add a blank line for readability
         const message = messageFactory();
         console.log(message);
-        console.log(''); // Add a blank line for readability
+        console.log('');
         // Continue prompting the user
         promptUser(rl);
     } else if (input === 'exit') {
         // Display goodbye message and close the readline interface
-        console.log(''); // Add a blank line for readability
+        console.log('');
         console.log('Goodbye!');
         rl.close();
     } else {
         // Inform the user of invalid input
+        console.log('');
         console.log('Invalid input. Please type "generate" to generate a message or "exit" to exit the program.');
-        // Continue prompting the user
+        console.log(''); 
         promptUser(rl);
     }
 };
-
 // Function to prompt the user for input
 const promptUser = (rl) => {
     // Prompt the user with a question
     rl.question('Type "generate" to generate a message or "exit" to exit the program: ', (input) => {
         // Convert input to lowercase
         input = input.toLowerCase();
-
         // Process the user input
         userInput(input, rl);
     });
 };
-
 // Create readline interface
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-
 // Start the program by calling promptUser
 promptUser(rl);
