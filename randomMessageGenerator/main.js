e5020cd86374dbc43ef381af9aac2e9b62eaec80
Andrew Rawson <arawson@proton.me>
2025-03-07 22:43:55 -0500

    commented up the main.js

---
+++ randomMessageGenerator/main.js
@@ -0,0 +1,13 @@
+// the idea is to create some time of random message generator in JS
+// I still need to figure out what context I want to use this in
+// Maybe a chat bot or a fortune cookie generator or some kind of 
+// quote generator or inspirational message generator, etc
+// as we can see, the possibilities are vast
+// The messages have to be comprised of three parts selected at random
+// It's supposed to demonstrate JS basics, so the data structure should be arrays
+// I may use some kind of factory function to generate messages
+// I also need to consider interactivity. How will the user interact with the program
+// Do I want them to use a prompt? Or enter input in a form?
+// It will be a terminal program, because we are working with basic concepts
+// This makes me want to lean toward a prompt
+// It also could just run from initialization without user input, but that seems less fun
